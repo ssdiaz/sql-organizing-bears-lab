4a670f0a4917feb6d83737fb503deac6656f1ae6
Samantha Diaz <samidiaz3@gmail.com>
2021-09-23 19:19:05 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name,  age,  sex,  color,  temperament,  alive) VALUES ("Mr. Choclate", 5, "M", "brown", "happy", 1); 
+INSERT INTO bears (name,  age,  sex,  color,  temperament,  alive) VALUES ("Rowdy", 2, "M", "purple", "energetic", 1);
+INSERT INTO bears (name,  age,  sex,  color,  temperament,  alive) VALUES ("Tabitha", 5, "F", "red", "snobby", 1);
+INSERT INTO bears (name,  age,  sex,  color,  temperament,  alive) VALUES ("Sergeant Brown", 10, "F", "green", "strict", 1);
+INSERT INTO bears (name,  age,  sex,  color,  temperament,  alive) VALUES ("Melissa", 18, "F", "yellow", "cheerful", 1);
+INSERT INTO bears (name,  age,  sex,  color,  temperament,  alive) VALUES ("Grinch", 5, "M", "green", "mean", 1); 
+INSERT INTO bears (name,  age,  sex,  color,  temperament,  alive) VALUES ("Wendy", 5, "M", "orange", "happy", 1);
+INSERT INTO bears (name,  age,  sex,  color,  temperament,  alive) VALUES (NULL, 5, "M", "brown", "happy", 0);
+-- boolean 1 is true, 0 is false
